79c30efd100269327f6c0eb406de4b080dc688a1
Вова <vovan_lipkin@mail.ru>
2020-08-08 02:20:18 +0300

    New json parser

--- src/main/java/com/example/vkbot/Controller.java
+++ src/main/java/com/example/vkbot/Controller.java
@@ -3,7 +3,6 @@ package com.example.vkbot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,7 +11,7 @@ public class Controller {
     @Autowired
     private MessageService messageService;
 
-    @RequestMapping
+    @PostMapping
     public String getMessage(@RequestBody String request) {
         return messageService.newAction(request);
     }
--- src/main/java/com/example/vkbot/MessageService.java
+++ src/main/java/com/example/vkbot/MessageService.java
@@ -1,9 +1,7 @@
 package com.example.vkbot;
 
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,11 +9,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class MessageService {
 
-    private final String OK_REPLY = "ok";
-    private final String CONFIRMATION_CODE = "44fdf7ec";
-    private final String TOKEN = "51e3815121931aac6e60e575655aced1707d4d336fd166c71e5fe2c2018c47199a4d3933265af04ef22be";
-
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
 
     public MessageService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
@@ -23,33 +17,33 @@ public class MessageService {
 
     public String newAction(String request){
 
-        String returnString = "";
+        String OK_REPLY = "ok";
+        String CONFIRMATION_CODE = "0a4a3be3";
+        String TOKEN = "51e3815121931aac6e60e575655aced1707d4d336fd166c71e5fe2c2018c47199a4d3933265af04ef22be";
 
-        try {
-            JSONObject jsonObject = (JSONObject) new JSONParser().parse(request);
+        String returnString;
 
-            switch ((String) jsonObject.get("type")) {
-                case ("confirmation"):
-                    returnString = CONFIRMATION_CODE;
-                    break;
-                case ("message_new"):
-                    JSONObject object = (JSONObject) new JSONParser().parse(String.valueOf(jsonObject.get("object")));
-                    System.out.println(jsonObject.get("object"));
-                    JSONObject messageObject = (JSONObject) new JSONParser().parse(String.valueOf(object.get("message")));
-                    String text = (String) messageObject.get("text");
-                    Long fromId = (Long) messageObject.get("from_id");
-                    System.out.printf("text = %s, from_id = %d", text, fromId);
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(request, JsonObject.class);
 
-                    String url = String.format("https://api.vk.com/method/messages.send?user_ids=%d&message=%s&access_token=%s&v=5.122&random_id=0", fromId, text, TOKEN);
-                    System.out.println(restTemplate.getForObject(url, String.class));
+        switch (jsonObject.get("type").getAsString()) {
+            case ("confirmation"):
+                returnString = CONFIRMATION_CODE;
+                break;
+            case ("message_new"):
+                JsonObject messageObject = jsonObject.get("object").getAsJsonObject().get("message").getAsJsonObject();
+                String text = messageObject.get("text").getAsString();
+                Long fromId = messageObject.get("from_id").getAsLong();
 
-                    returnString = OK_REPLY;
-            }
+                String url = String.format("https://api.vk.com/method/messages.send?user_ids=%d&message=%s&access_token=%s&v=5.122&random_id=0", fromId, text, TOKEN);
+                restTemplate.getForObject(url, String.class);
 
-        } catch (ParseException e) {
-            e.printStackTrace();
+                returnString = OK_REPLY;
+                break;
+            default:
+                returnString = null;
         }
-        System.out.println(returnString);
+
         return returnString;
     }
 }
